24f759953099eb4ada1ace718a1fd1a36a425194
Weronika Kansy <weronika.kansy@gmail.com>
2021-01-09 00:41:26 +0100

    IDs added to HTML elements; new time container added in h3; variables with reference to HTML elements declared in JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
 console.log('Good luck!')
+
+const startBtn = document.querySelector("#start-btn");
+const nextBtn = document.querySelector("#next-btn");
+const stopBtn = document.querySelector("#stop-btn");
+const pauseBtn = document.querySelector("#pause-btn");
+const saveBtn = document.querySelector("#save-btn");
+const resetBtn = document.querySelector("#reset-btn");
+const timeContainer = document.querySelector("#time-container");
+const previousTimeContainer = document.querySelector("#previous-time-container");
+const resultsContainer = document.querySelector("#results-container");

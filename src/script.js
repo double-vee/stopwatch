b5c635f49fc973975589397fd88129a607c16ec9
Weronika Kansy <weronika.kansy@gmail.com>
2021-01-09 01:02:16 +0100

    Event handler added to start btn: timer started on click, btns disabled/enabled/unhidden

--- src/script.js
+++ src/script.js
@@ -15,3 +15,31 @@ saveBtn.hidden = true;
 previousTimeContainer.hidden = true;
 pauseBtn.disabled = true;
 nextBtn.disabled = true;
+
+let startTime;
+let createTimer;
+let time;
+let seconds;
+
+startBtn.addEventListener('click', () => {
+  startTime = Date.now();
+  createTimer = setInterval(function() {
+    time = Date.now() - startTime;
+    seconds = Math.floor(time / 1000);
+    console.log(time, seconds);
+
+    formatTime();
+  }, 1000);
+  
+  startBtn.disabled = true;
+  pauseBtn.disabled = false;
+  nextBtn.disabled = false;
+  stopBtn.hidden = false;
+});
+
+function formatTime() {
+  let min = Math.floor(seconds / 60);
+  let sec = seconds % 60;
+
+  return timeContainer.textContent = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
+}
